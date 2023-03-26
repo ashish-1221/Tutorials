36f2ba00e6a21cd969fa85663357d3032e62ac22
ashish-1221 <46136571+ashish-1221@users.noreply.github.com>
2023-03-26 12:45:20 +0530

    Tutorial for Java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Function is a collection of code that we can use again and again
+// Name of file must be same as name of 
+// public class
+// Word Main is reserved
+// java 
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!!");
+    }
+}
